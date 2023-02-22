144d27613bbe38dedb790178157960bbc280e579
yamlakkassahun <yamlak.k@gmail.com>
2023-02-22 14:19:46 +0300

    update the hint text on index.js file

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ app.get('/', (req, res, next) => {
     try {
         const user = req.body.user;
         if (!user) { 
-            // res.status(404).send({ status: 404, message: 'User Not Found' });
-            throw new Error("User not found")
+            res.status(404).send({ status: 404, message: 'User Not Found' });
+            // throw new Error("User not found")
         }
     } catch (error) { 
         return next(error);
@@ -23,4 +23,4 @@ app.get('/', (req, res, next) => {
 
 app.use(ErrorMiddleware)
 
-app.listen(port, () => console.log(`Server is listening on port ${port}!`))
+app.listen(port, () => console.log(`${process.env.NODE_ENV.toUpperCase()} Server is listening on port ${port}!`))
